816c6e5e6e289874ff42812809fbe64c00ec27c6
Bhanu P <bhannushankar422@gmail.com>
2025-01-23 11:52:20 -0600

    added io reading

---
+++ ioreading.py
@@ -0,0 +1,9 @@
+# load inputs from console and print them
+
+# Read input from the console
+input1 = input("Enter first input: ")
+input2 = int(input("Enter second int input: "))
+
+# Print the inputs
+print("First input:", input1)
+print("Second input:", input2)
